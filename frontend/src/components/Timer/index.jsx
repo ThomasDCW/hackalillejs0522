4d34ccf5f977c2b41908f25820a3cb93a7e29f7c
Ilalande <jcasier@protonmail.com>
2022-05-12 16:46:18 +0200

    creating v1 of timer component (not functionning)

--- frontend/src/components/Timer/index.jsx
+++ frontend/src/components/Timer/index.jsx
@@ -1,5 +1,13 @@
 import STimer from "./style";
+import timerImg from "../../assets/timer.png";
+// import context from "../../../context/Ctx";
 
 export default function Wallet() {
-  return <STimer>I'm the timer</STimer>;
+  const timerActif = true; // à remplacer par donnée du contexte
+  return (
+    <STimer>
+      <img src={timerImg} />
+      <p className={timerActif ? "showTimer" : "hideTimer"}>00:05</p>
+    </STimer>
+  );
 }
--- frontend/src/components/Timer/style.jsx
+++ frontend/src/components/Timer/style.jsx
@@ -3,4 +3,12 @@ import styled from "styled-components";
 export default styled.div`
   height: 10vh;
   background-color: gold;
+
+  img {
+    max-width: 200px;
+    width: 5vw;
+  }
+  .hideTimer {
+    display: none;
+  }
 `;
