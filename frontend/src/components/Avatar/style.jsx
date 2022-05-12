31d0704ad5f8cdf4f1bf88fe2ba1a48ba675c8af
MassimaMao <mastem@msn.com>
2022-05-12 17:02:53 +0200

    fix confilcts

--- frontend/src/components/Avatar/style.jsx
+++ frontend/src/components/Avatar/style.jsx
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export default styled.div`
   height: 40vh;
-  background-color: violet;
+  background-color: #423e42;
+  background-image: url("src/assets/avatar.png");
 `;
