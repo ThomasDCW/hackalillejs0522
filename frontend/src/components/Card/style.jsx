a93ce5669db7d948c9ecb31e5006f8eaf58b3411
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-12 18:43:48 +0200

    fix card and style

--- frontend/src/components/Card/style.jsx
+++ frontend/src/components/Card/style.jsx
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
 export default styled.div`
+  height: 20rem;
+  width: 16rem;
+  background-color: white;
   box-sizing: border-box;
   border: 10px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 2vh;
   img {
-    width: 150px;
+    height: 180px;
+  }
+  h1 {
+    text-align: center;
+    font-weight: bold;
+  }
+  h2 {
+    text-align: center;
   }
 `;
