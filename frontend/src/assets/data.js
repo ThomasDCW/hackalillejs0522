cc43c2ed17e005ec72d76279e4787ea094e48c07
Jeremy <jeremy.jorand@gmail.com>
2022-05-12 15:14:35 +0200

    create data

---
+++ frontend/src/assets/data.js
@@ -0,0 +1,67 @@
+[
+  {
+  "nom":"Champ d'avocat",
+  "profit":"17000€/T",
+  "impact_ecologique":"550/t",
+  "image":"",
+  "categorie":"Champ",
+  "cout_achat":"10000",
+  "superficie":"1hectar€",
+  "production":"10T/an",
+  
+  },
+  {
+  "nom":"Champs de maïs",
+  "profit":"350€/T",
+  "impact_ecologique":"240/T",
+  "image":"",
+  "categorie":"Champ",
+  "cout_achat":"10000€",
+  "superficie":"1hectar",
+  "production":"1000T/an",
+  
+  },
+  {
+  "nom":"Ferme de volaille",
+  "profit":"1800€/T",
+  "impact_ecologique":"830/T",
+  "image":"",
+  "categorie":"Elevage",
+  "cout_achat":"10000€",
+  "superficie":"1hectar",
+  "production":"80T/an",
+  
+  },{
+  "nom":"Ferme de boeuf",
+  "profit":"6500€/T",
+  "impact_ecologique":"346/T",
+  "image":"",
+  "categorie":"Elevage",
+  "cout_achat":"10000€",
+  "superficie":"1hectar",
+  "production":"0.6T/an",
+  
+  },
+  ,{
+  "nom":"Champ de fraise",
+  "profit":"6000€/T",
+  "impact_ecologique":"420/T",
+  "image":"",
+  "categorie":"champ",
+  "cout_achat":"10000€",
+  "superficie":"1hectar",
+  "production":"34T/an",
+  
+  },
+  ,{
+  "nom":"Ferme de porc",
+  "profit":"1900€/T",
+  "impact_ecologique":"146/T",
+  "image":"",
+  "categorie":"Elevage",
+  "cout_achat":"10000€",
+  "superficie":"1hectar",
+  "production":"9T/an",
+  
+  }
+  ]
