80ab6df21b098a2098044a635236edd3b5a8ed7b
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-12 23:44:26 +0200

    add some style

--- frontend/src/components/Start/style.jsx
+++ frontend/src/components/Start/style.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 export default styled.div`
-  width: 10em;
   button {
     font-size: 50px;
-    background-color: #585ad4;
-    color: white;
-    border-radius: 10%;
+    box-shadow: 3px 4px 0px 0px #000000;
+    background: linear-gradient(to bottom, #35c61f 5%, #35c61f 100%);
+    background-color: #35c61f;
+    border-radius: 18px;
+    border: 1px solid #35c61f;
+    display: inline-block;
+    cursor: pointer;
+    color: #ffffff;
+    font-family: Arial;
+    padding: 7px 25px;
+    text-decoration: none;
+    text-shadow: 0px 1px 0px #35c61f;
     margin: 0.5em;
   }
   .hide {
