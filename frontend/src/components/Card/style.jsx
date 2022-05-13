112f328bbdcd365e594c021eb9b61055213f98c3
Jeremy <jeremy.jorand@gmail.com>
2022-05-13 12:26:31 +0200

    fic color and duration

--- frontend/src/components/Card/style.jsx
+++ frontend/src/components/Card/style.jsx
@@ -37,7 +37,7 @@ export default styled.div`
     width: 50%;
   }
   .plus {
-    background: linear-gradient(to bottom, #77b55a 5%, #72b352 100%);
+    background: linear-gradient(to bottom, #5ad79d 5%, #5ad79d 100%);
     background-color: #77b55a;
     border-radius: 4px;
     border: 1px solid #4b8f29;
