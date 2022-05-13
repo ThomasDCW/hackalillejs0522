6c1b37cd611b1e2d8a6e1711d0cc2a59796d16b1
Ilalande <jcasier@protonmail.com>
2022-05-13 09:39:56 +0200

    adding a endGameFunc in context. for the moment reset the timer and stop the game

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -4,7 +4,17 @@ import Avatar from "@components/Avatar";
 import Timer from "@components/Timer";
 import Start from "@components/Start";
 import Stop from "@components/Stop";
+import { useContext } from "react";
+
+import { useEffect } from "react";
+import StatsContext from "../services/contexts/index";
+
 export default function Game() {
+  const { timer, endGameFunc } = useContext(StatsContext);
+  useEffect(() => {
+    endGameFunc();
+  }, [timer]);
+
   return (
     <main>
       <section className="gallery">
