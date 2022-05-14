d8ec567705df3d319335c284ab72b385604cfea4
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-14 18:28:21 +0200

    fixfooter

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,10 +1,5 @@
 import SFooter from "./style";
-import logo from "../../assets/logoBigBill.png";
 
 export default function Header() {
-  return (
-    <SFooter>
-      <img src={logo} alt="logo" />
-    </SFooter>
-  );
+  return <SFooter></SFooter>;
 }
