1a8406ec618832c53d5155fc31688e7f4cfee6cd
ThomasDCW <thomas.dicola@hotmail.fr>
2022-05-14 18:22:19 +0200

    fix style on counter and timer

--- frontend/src/components/Timer/style.jsx
+++ frontend/src/components/Timer/style.jsx
@@ -9,14 +9,14 @@ export default styled.div`
   margin-left: 1vw;
 
   .timer {
-    font-size: 52px;
+    font-size: 20px;
     img {
       width: 70px;
     }
   }
   @media screen and (max-width: 600px) {
     .timer {
-      font-size: 32px;
+      font-size: 18px;
       margin-bottom: 0.5rem;
     }
   }
